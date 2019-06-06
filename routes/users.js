ce7ffef0cecc141198cc99425b0d069938135f53
王立广 <wangliguang@daling.com>
2019-06-06 17:32:05 +0800

    👍 FEAT: 跨版本patch服务

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const exec = require('child_process').exec;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/getPatch', function(req, res, next) {
+  const params = req.query
+  console.log(params.bundleVersion);
+  
+
+  
+  var cmdStr = 'cd ./public/bundle && ls -t * | head -1';
+  exec(cmdStr, function(err,stdout,stderr){
+    if(err) {
+      console.log('get weather api error:'+stderr);
+    } else {
+      res.json({ name: stdout});
+    }
+  });
+
+  
 });
 
 module.exports = router;
