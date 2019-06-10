767990c1ac7f85a1e33b560b050762c849401109
王立广 <wangliguang@daling.com>
2019-06-10 09:40:16 +0800

    👍 FEAT: 跨版本产生差分包

---
+++ routes/tools.js
@@ -0,0 +1,18 @@
+const exec = require('child_process').exec;
+
+
+const CMDPromise = function(cmdStr) {
+  return new Promise((resolve, reject) => {
+    exec(cmdStr, function(err,stdout,stderr){
+      if(err) {
+        reject(err);
+        return;
+      }
+      resolve(stdout.replace(/[\r\n]/g,""));
+    });
+  })
+}
+
+module.exports = {
+  CMDPromise
+};
--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,35 @@
 var express = require('express');
 var router = express.Router();
-const exec = require('child_process').exec;
+var { CMDPromise } = require('./tools');
 
-/* GET users listing. */
-router.get('/getPatch', function(req, res, next) {
-  const params = req.query
-  console.log(params.bundleVersion);
-  
 
-  
-  var cmdStr = 'cd ./public/bundle && ls -t * | head -1';
-  exec(cmdStr, function(err,stdout,stderr){
-    if(err) {
-      console.log('get weather api error:'+stderr);
-    } else {
-      res.json({ name: stdout});
+const BUNDLE_URL_PREFIX = 'http://47.94.81.19:3000/bundle/';
+const PATCH_URL_PREFIX = 'http://47.94.81.19:3000/patch/';
+
+router.get('/getPatch', async function(req, res, next) {
+  try {
+    const { currentBundleVersion } = req.query;
+    let cmdStr = 'cd ./public/bundle && ls -t * | head -1';
+    const latestBundle = await CMDPromise(cmdStr);
+    if(currentBundleVersion == 0) {
+      res.json({ 
+        bundleUrl: `${BUNDLE_URL_PREFIX}${latestBundle}`,
+        code: 200,
+      });
+      return;
     }
-  });
+
+    await CMDPromise(`diff ./public/bundle/${currentBundleVersion}.bundle ./public/bundle/${latestBundle} > ./public/patch/diff.patch`);
+    res.json({
+      code: 200,
+      patchUrl: `${PATCH_URL_PREFIX}diff.patch`
+    });
+  } catch (error) {
+    res.json(error);
+  }
+
+  
+  
 
   
 });
