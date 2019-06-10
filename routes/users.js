dfdaff04e077c63cab1647f574c78bb50899b1de
王立广 <wangliguang@daling.com>
2019-06-10 11:10:15 +0800

    👍 FEAT: 路径高错误了

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ const PATCH_URL_PREFIX = 'http://47.94.81.19:3000/patch/';
 router.get('/getPatch', async function(req, res, next) {
   try {
     const { currentBundleVersion } = req.query;
-    let cmdStr = 'cd ./public/bundle && ls -t * | head -1';
+    let cmdStr = 'cd ../public/bundle && ls -t * | head -1';
     const latestBundle = await CMDPromise(cmdStr);
     if(currentBundleVersion == 0) {
       res.json({ 
@@ -19,7 +19,7 @@ router.get('/getPatch', async function(req, res, next) {
       return;
     }
 
-    await CMDPromise(`diff ./public/bundle/${currentBundleVersion}.bundle ./public/bundle/${latestBundle} > ./public/patch/diff.patch`);
+    await CMDPromise(`diff ../public/bundle/${currentBundleVersion}.bundle ../public/bundle/${latestBundle} > ./public/patch/diff.patch`);
     res.json({
       code: 200,
       patchUrl: `${PATCH_URL_PREFIX}diff.patch`
